addb96b2aaf57bdaeeecf0c176c4bca2ec3281b3
Thomas Fischer <thomas@confbase.com>
2018-05-05 21:35:59 -0500

    add prettify, json schema support

--- infer/infer.go
+++ infer/infer.go
@@ -5,25 +5,40 @@ import (
 	"fmt"
 	"io"
 	"os"
+
+	"github.com/Confbase/schema/schema"
 )
 
-func Infer(args []string) {
+func InferEntry(cfg Config, args []string) {
 	if len(args) == 0 {
-		infer(os.Stdin, os.Stdout)
+		Infer(os.Stdin, os.Stdout, cfg)
 	} else {
 		fmt.Fprintf(os.Stderr, "error: not implemented yet\n")
 		os.Exit(1)
 	}
 }
 
-func infer(r io.Reader, w io.Writer) {
+func Infer(r io.Reader, w io.Writer, cfg Config) {
 	var i interface{}
 	if err := json.NewDecoder(r).Decode(&i); err != nil {
 		fmt.Fprintf(os.Stderr, "error: failed to parse JSON\n")
 		os.Exit(1)
 	}
-	if err := json.NewEncoder(w).Encode(&i); err != nil {
-		fmt.Fprintf(os.Stderr, "error: failed to write JSON to stdout\n")
+
+	data, ok := i.(map[string]interface{})
+	if !ok {
+		fmt.Fprintf(os.Stderr, "error: data is in unrecognized format\n")
+		os.Exit(1)
+	}
+
+	s := schema.New(data)
+	js, err := s.ToJsonSchema(cfg.DoMakeRequired)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to infer schema :/\n")
+		os.Exit(1)
+	}
+	if err := js.Serialize(w, cfg.DoPretty); err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to serialize schema\n%v", err)
 		os.Exit(1)
 	}
 }
