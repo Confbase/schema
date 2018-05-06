b3151d9fb0a89c24068c82e7d2d1c5b4a7426e15
Thomas Fischer <thomas@confbase.com>
2018-05-05 23:17:24 -0500

    add yaml support

--- infer/infer.go
+++ infer/infer.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/Confbase/schema/schema"
 )
@@ -19,15 +22,9 @@ func InferEntry(cfg Config, args []string) {
 }
 
 func Infer(r io.Reader, w io.Writer, cfg Config) {
-	var i interface{}
-	if err := json.NewDecoder(r).Decode(&i); err != nil {
-		fmt.Fprintf(os.Stderr, "error: failed to parse JSON\n")
-		os.Exit(1)
-	}
-
-	data, ok := i.(map[string]interface{})
-	if !ok {
-		fmt.Fprintf(os.Stderr, "error: data is in unrecognized format\n")
+	data, err := readToMap(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
@@ -37,8 +34,32 @@ func Infer(r io.Reader, w io.Writer, cfg Config) {
 		fmt.Fprintf(os.Stderr, "error: failed to infer schema :/\n")
 		os.Exit(1)
 	}
+
 	if err := js.Serialize(w, cfg.DoPretty); err != nil {
 		fmt.Fprintf(os.Stderr, "error: failed to serialize schema\n%v", err)
 		os.Exit(1)
 	}
+}
+
+func readToMap(r io.Reader) (map[string]interface{}, error) {
+	// ReadAll is necessary, since the input stream could be only
+	// traversable once; we must be sure to save the data
+	// into a buffer on the first pass, so that we can read it
+	// *multiple* times
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make(map[string]interface{})
+	if err = json.Unmarshal(buf, &data); err == nil {
+		return data, nil
+	}
+
+	data = make(map[string]interface{}) // be sure it's an empty map
+	if err = yaml.Unmarshal(buf, &data); err == nil {
+		return data, nil
+	}
+
+	return nil, fmt.Errorf("failed to recognize input data format")
 }
