b51b91084695293c0221d510ef8ec74f3a7d0b84
Thomas Fischer <thomas@confbase.com>
2018-05-06 10:04:12 -0500

    add infer yaml e2e tests, clean up README

--- e2e_tests/infer.sh
+++ e2e_tests/infer.sh
@@ -257,6 +257,185 @@ infer_json_array_of_array_objects() {
 }'
 }
 
+infer_yaml_string() {
+    output=`printf 'color: red' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "color": {
+            "type": "string"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_positive_integer() {
+    output=`printf 'myNumber: 12' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "myNumber": {
+            "type": "number"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_negative_integer() {
+    output=`printf 'myNumber: -12310' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "myNumber": {
+            "type": "number"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_positive_float() {
+    output=`printf 'myNumber: 420.6' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "myNumber": {
+            "type": "number"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_negative_float() {
+    output=`printf 'myNumber: -1902.32249' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "myNumber": {
+            "type": "number"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_zero() {
+    output=`printf 'myNumber: 0' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "myNumber": {
+            "type": "number"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_boolean() {
+    output=`printf 'is2004: true' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "is2004": {
+            "type": "boolean"
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_array_of_strings() {
+    output=`printf 'people: ["bladee","thomas"]' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "people": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_array_of_numbers() {
+    output=`printf 'ages: [12.1,-1,43,-2.3,0,-0,0.0]' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "ages": {
+            "type": "array",
+            "items": {
+                "type": "number"
+            }
+        }
+    },
+    "required": []
+}'
+}
+
+infer_yaml_array_of_booleans() {
+    output=`printf 'truthinesses: [true,false,false]' | schema infer 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{
+    "title": "",
+    "type": "object",
+    "properties": {
+        "truthinesses": {
+            "type": "array",
+            "items": {
+                "type": "boolean"
+            }
+        }
+    },
+    "required": []
+}'
+}
+
 tests=(
     "infer_unrecognized_format"
     "infer_json_minimal"
@@ -272,4 +451,14 @@ tests=(
     "infer_json_array_of_booleans"
     "infer_json_array_of_objects"
     "infer_json_array_of_array_objects"
+    "infer_yaml_string"
+    "infer_yaml_positive_integer"
+    "infer_yaml_negative_integer"
+    "infer_yaml_positive_float"
+    "infer_yaml_negative_float"
+    "infer_yaml_zero"
+    "infer_yaml_boolean"
+    "infer_yaml_array_of_strings"
+    "infer_yaml_array_of_numbers"
+    "infer_yaml_array_of_booleans"
 )
