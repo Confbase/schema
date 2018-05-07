0bd5bbb8c39f021203f41b60948b258af66bf4c3
Thomas Fischer <thomas@confbase.com>
2018-05-07 15:53:49 -0500

    fix obj in array bug; close #17

--- e2e_tests/lib.sh
+++ e2e_tests/lib.sh
@@ -12,10 +12,24 @@ run_test() {
         exit 1
     fi
 
-    difference="$(diff <(printf "%s" "$expect") <(printf "%s" "$output"))"
-    if [ ! -z "$difference" ]; then
-        printf "FAIL. Expected this output:\n%s\nBut got:\n%s" "$expect" "$output"
-        exit 1
+    if [ "$expect_either_or" = 'true' ]; then
+        diffe="$(diff <(printf "%s" "$expect_either") <(printf "%s" "$output"))"
+        diffo="$(diff <(printf "%s" "$expect_or")     <(printf "%s" "$output"))"
+        if [ ! -z "$diffe" ] && [ ! -z "$diffo" ]; then
+            printf "FAIL. Expected either:\n%s\n" "$expect_either"
+            printf "or:\n%s\n" "$expect_or"
+            printf "but got:\n%s\n" "$output"
+            printf "diff (either):\n%s\n" "$diffe"
+            printf "diff (or):\n%s\n" "$diffo"
+            exit 1
+        fi
+        expect_either_or='false'
+    else
+        difference="$(diff <(printf "%s" "$expect") <(printf "%s" "$output"))"
+        if [ ! -z "$difference" ]; then
+            printf "FAIL. Expected output:\n%s\nBut got:\n%s" "$expect" "$output"
+            exit 1
+        fi
     fi
     printf "OK\n"
 }
