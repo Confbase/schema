4d331d86b622a3e3f3a5c705400d5d88f029c759
Thomas Fischer <thomas@confbase.com>
2018-05-07 22:05:43 -0500

    add yaml, toml support to 'schema init'

--- e2e_tests/init.sh
+++ e2e_tests/init.sh
@@ -80,6 +80,123 @@ init_json_nested_object() {
 }'
 }
 
+init_yaml_minimal() {
+    output=`printf '{}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='{}'
+}
+
+init_yaml_string() {
+    output=`printf '{"name": "Thomas"}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='name: ""'
+}
+
+init_yaml_number() {
+    output=`printf '{"age": 20}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='age: 0'
+}
+
+init_yaml_boolean() {
+    output=`printf '{"isHandsome": true}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='isHandsome: false'
+}
+
+init_yaml_null() {
+    output=`printf '{"badField": null}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='badField: null'
+}
+
+init_yaml_array() {
+    output=`printf '{"truthfulnesses": [true,false,false,true]}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='truthfulnesses: []'
+}
+
+init_yaml_nested_object() {
+    output=`printf '{"myObj": {"field1":1,"field2":"Finland"}}' | schema infer | schema init --yaml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='myObj:
+  field1: 0
+  field2: ""'
+}
+
+init_toml_minimal() {
+    output=`printf '{}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect=''
+}
+
+init_toml_string() {
+    output=`printf '{"name": "Thomas"}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='name = ""'
+}
+
+init_toml_number() {
+    output=`printf '{"age": 20}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='age = 0'
+}
+
+init_toml_boolean() {
+    output=`printf '{"isHandsome": true}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='isHandsome = false'
+}
+
+init_toml_null() {
+    output=`printf '{"badField": null}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='1'
+    expect='error: failed to serialize instance of schema
+toml: cannot marshal nil interface {}'
+}
+
+init_toml_array() {
+    output=`printf '{"truthfulnesses": [true,false,false,true]}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='truthfulnesses = []'
+}
+
+init_toml_nested_object() {
+    output=`printf '{"myObj": {"field1":1,"field2":"Finland"}}' | schema infer | schema init --toml 2>&1`
+    status="$?"
+
+    expect_status='0'
+    expect='[myObj]
+field1 = 0
+field2 = ""'
+}
+
 tests=(
     "init_invalid_schema"
     "init_json_minimal"
@@ -89,4 +206,18 @@ tests=(
     "init_json_null"
     "init_json_array"
     "init_json_nested_object"
+    "init_yaml_minimal"
+    "init_yaml_string"
+    "init_yaml_number"
+    "init_yaml_boolean"
+    "init_yaml_null"
+    "init_yaml_array"
+    "init_yaml_nested_object"
+    "init_toml_minimal"
+    "init_toml_string"
+    "init_toml_number"
+    "init_toml_boolean"
+    "init_toml_null"
+    "init_toml_array"
+    "init_toml_nested_object"
 )
---
+++ initcmd/config.go
@@ -0,0 +1,34 @@
+package initcmd
+
+type Config struct {
+	SchemaPath string
+	DoJson     bool
+	DoYaml     bool
+	DoToml     bool
+	DoXml      bool
+	DoProtobuf bool
+	DoGraphQL  bool
+	DoPretty   bool
+}
+
+func (cfg *Config) OutFmt() string {
+	if cfg.DoJson {
+		return "json"
+	}
+	if cfg.DoYaml {
+		return "yaml"
+	}
+	if cfg.DoToml {
+		return "toml"
+	}
+	if cfg.DoXml {
+		return "xml"
+	}
+	if cfg.DoProtobuf {
+		return "protobuf"
+	}
+	if cfg.DoGraphQL {
+		return "graphql"
+	}
+	return "json"
+}
