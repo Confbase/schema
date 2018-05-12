8d3f2f51de504b5ab67ddded41d1c6f74514f0b4
Hoobadooba <Hoobadooba4@yahoo.com>
2018-05-11 20:40:31 -0500

    Add Translate Command

---
+++ cmd/translate.go
@@ -0,0 +1,52 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+
+	"github.com/spf13/cobra"
+
+	"github.com/Confbase/schema/translate"
+)
+
+var translateCfg translate.Config
+
+// translateCmd represents the translate command
+var translateCmd = &cobra.Command{
+	Use:   "translate",
+	Short: "translate input into desired type",
+	Long: `translate input into desired type
+If no schema is specified, stdin is interpreted as the schema.
+
+Multiple instance names may be specfied.
+
+If more than one of the (json|yaml|toml|xml|protobuf|graphql) flags are set,
+behavior is undefined.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		translate.TranslateEntry(translateCfg, args)
+	},
+}
+
+func init() {
+	translateCmd.Flags().StringVarP(&translateCfg.SchemaPath, "schema", "s", "", "specifies schema to init")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoJson, "json", "", false, "initialize as JSON")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoYaml, "yaml", "", false, "initialize as YAML")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoToml, "toml", "", false, "initialize as TOML")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoXml, "xml", "", false, "initialize as XML")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoProtobuf, "protobuf", "", false, "initialize as protocol buffer")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoGraphQL, "graphql", "", false, "initialize as GraphQL instance")
+	translateCmd.Flags().BoolVarP(&translateCfg.DoPretty, "pretty", "", true, "pretty-print the output")
+	RootCmd.AddCommand(translateCmd)
+}
---
+++ translate/config.go
@@ -0,0 +1,34 @@
+package translate
+
+type Config struct {
+	SchemaPath string
+	DoJson     bool
+	DoYaml     bool
+	DoToml     bool
+	DoXml      bool
+	DoProtobuf bool
+	DoGraphQL  bool
+	DoPretty   bool
+}
+
+func (translate *Config) OutFmt() string {
+	if translate.DoJson {
+		return "json"
+	}
+	if translate.DoYaml {
+		return "yaml"
+	}
+	if translate.DoToml {
+		return "toml"
+	}
+	if translate.DoXml {
+		return "xml"
+	}
+	if translate.DoProtobuf {
+		return "protobuf"
+	}
+	if translate.DoGraphQL {
+		return "graphql"
+	}
+	return "json"
+}
